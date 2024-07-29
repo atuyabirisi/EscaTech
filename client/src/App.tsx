f6fa5a0f0863372362bdf3dafe3e09076b9a3d4b
birisi atuya <tushhamphrey@gamil.com>
2024-07-29 15:59:20 +0300

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <>
+      <div className="d-flex justify-content-center">
+        <h3 className="text-danger">RTVC MIS</h3>
+      </div>
+    </>
+  );
+}
