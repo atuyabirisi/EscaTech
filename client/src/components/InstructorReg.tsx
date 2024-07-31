37b093e4dea473ed577b601b2c8c5a206d2201df
birisi atuya <tushhamphrey@gamil.com>
2024-07-31 18:40:43 +0300

    add style to instructor registration form

--- client/src/components/InstructorReg.tsx
+++ client/src/components/InstructorReg.tsx
@@ -5,11 +5,10 @@ export default function InstructorReg() {
 
   return (
     <div className="row mx-1">
-      <div className="col-lg-3"></div>
+      <div className="d-flex justify-content-center py-3">
+        <h5>Register New Instructor</h5>
+      </div>
       <div className="col-lg-6">
-        <div className="d-flex justify-content-center py-3">
-          <h5>Register New Instructor</h5>
-        </div>
         <form onSubmit={handleSubmit((data) => console.log(data))}>
           <div className="mb-3">
             <label htmlFor="fname" className="form-label">
@@ -69,65 +68,66 @@ export default function InstructorReg() {
               className="form-control"
             />
           </div>
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">
-              Email
-            </label>
-            <input
-              {...register("email")}
-              id="email"
-              type="text"
-              className="form-control"
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="a/c" className="form-label">
-              Bank Account No
-            </label>
-            <input
-              {...register("a/c")}
-              id="a/c"
-              type="number"
-              className="form-control"
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="kra" className="form-label">
-              KRA Pin
-            </label>
-            <input
-              {...register("kra")}
-              id="kra"
-              type="text"
-              className="form-control"
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="dep" className="form-label">
-              Department
-            </label>
-            <select {...register("dep")} id="dep" className="form-control">
-              <option value=""></option>
-              <option value="Information Systems & Computing">
-                Information Systems & Computing
-              </option>
-              <option value="Building & Civil Engineering">
-                Building & Civil Engineering
-              </option>
-              <option value="Mechanical Engineering">
-                Mechanical Engineering
-              </option>
-              <option value="Liberal Studies">Liberal Studies</option>
-            </select>
-          </div>
-          <div className="mb-3 d-flex justify-content-end">
-            <button type="submit" className="btn btn-primary">
-              Register
-            </button>
-          </div>
         </form>
       </div>
-      <div className="col-lg-3"></div>
+      <div className="col-lg-6">
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+            {...register("email")}
+            id="email"
+            type="text"
+            className="form-control"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="a/c" className="form-label">
+            Bank Account No
+          </label>
+          <input
+            {...register("a/c")}
+            id="a/c"
+            type="number"
+            className="form-control"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="kra" className="form-label">
+            KRA Pin
+          </label>
+          <input
+            {...register("kra")}
+            id="kra"
+            type="text"
+            className="form-control"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="dep" className="form-label">
+            Department
+          </label>
+          <select {...register("dep")} id="dep" className="form-control">
+            <option value=""></option>
+            <option value="Information Systems & Computing">
+              Information Systems & Computing
+            </option>
+            <option value="Building & Civil Engineering">
+              Building & Civil Engineering
+            </option>
+            <option value="Mechanical Engineering">
+              Mechanical Engineering
+            </option>
+            <option value="Liberal Studies">Liberal Studies</option>
+          </select>
+        </div>
+        <div className="mb-3 d-flex justify-content-end">
+          <button type="submit" className="btn btn-primary">
+            Register
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
