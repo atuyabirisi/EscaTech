613919ab61220740817f8ca6fe5c9d1463dd64c8
birisi atuya <tushhamphrey@gamil.com>
2024-07-31 21:37:28 +0300

    initial server configuration and connection to mongodb

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect("mongodb://0.0.0.0:27017/rtvc")
+  .then(() => console.log("connected to mongodb"))
+  .catch((error) => console.log(error));
+
+const port = 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
